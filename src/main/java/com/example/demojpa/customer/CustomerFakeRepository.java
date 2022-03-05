185e3ad057b44f8f6ae7882bb35550c011f18b8f
jasonx <jason@mediapoint.com.au>
2022-03-05 15:46:40 +1100

    Commit

--- src/main/java/com/example/demojpa/customer/CustomerFakeRepository.java
+++ src/main/java/com/example/demojpa/customer/CustomerFakeRepository.java
@@ -4,14 +4,18 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.List;
 
-public class CustomerRepository implements CustomerRepo {
+public class CustomerFakeRepository implements CustomerRepo{
+
     @Override
     public List<Customer> getCustomers() {
+        return Arrays.asList(
+                new Customer(1L,"Jason Bond"),
+                new Customer(2L,"Jamie Bond"),
+                new Customer(3L,"David Bond")
+                );
 
-        // TODO connect to real db
-        return Collections.emptyList();
     }
 }
---
+++ src/main/java/com/example/demojpa/customer/CustomerRepo.java
@@ -0,0 +1,7 @@
+package com.example.demojpa.customer;
+
+import java.util.List;
+
+public interface CustomerRepo {
+    List<Customer> getCustomers();
+}
