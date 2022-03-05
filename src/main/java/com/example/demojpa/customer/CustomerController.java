b0e1c5b6c5069dbac342186c7c67d694c4d2e126
jasonx <jason@mediapoint.com.au>
2022-03-05 14:09:54 +1100

    Commit

--- src/main/java/com/example/demojpa/customer/CustomerController.java
+++ src/main/java/com/example/demojpa/customer/CustomerController.java
@@ -1,15 +1,26 @@
 package com.example.demojpa.customer;
 
 import com.example.demojpa.DemoJpaApplication;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
 
+
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
-    Customer getCustomer(){
-        return new Customer(1L,"Jason Bond");
+    List<Customer> getCustomer(){
+        return customerService.getCustomer();
     }
 
 }
--- src/main/java/com/example/demojpa/customer/CustomerService.java
+++ src/main/java/com/example/demojpa/customer/CustomerService.java
@@ -1,4 +1,24 @@
 package com.example.demojpa.customer;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class CustomerService {
+
+    private final CustomerRepo customerRepo;
+
+    public CustomerService(@Qualifier("fake") CustomerRepo customerRepo) {
+        this.customerRepo = customerRepo;
+    }
+
+
+    public List<Customer> getCustomer(){
+            return customerRepo.getCustomers();
+    }
+
+
+
 }
